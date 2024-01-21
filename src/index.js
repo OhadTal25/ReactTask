3cb956e2a5187aef924f39a648191ab301538aa9
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-01-21 13:09:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+// Brings in the React library for building the UI.
 import React from 'react';
+// Imports the createRoot method to manage the root of the React component tree.
 import { createRoot } from 'react-dom/client';  // Import createRoot
+// Imports the App component, the main component of your app.
 import App from './App.js';
+// Brings in CSS styling for global styles.
 import './index.css';
 
+
+// Finds the HTML element with the id 'root'.
 const container = document.getElementById('root');
+// Initializes the root for the React application.
 const root = createRoot(container);  // Create a root
 
+
 root.render(
+  //Renders the App component inside React's root, 
+  //wrapped in React.StrictMode for extra checks and warnings in development.
   <React.StrictMode>
     <App />
   </React.StrictMode>
