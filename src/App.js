894cb74b9b4fb645b136e64b97a71622a0c19c67
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-01-21 12:53:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lobby from './components/Lobby.js';
 import CodeBlock from './components/CodeBlock.js';
-import MyPage from './components/MyPage.js'; 
+import MyPage from './components/CodeEditor.js'; 
 import './App.css'; 
 import './components/Lobby.css'; 
 import './components/CodeBlock.css'; 
@@ -14,7 +14,7 @@ const App = () => {
               <Routes>
                   <Route path="/" element={<Lobby />} />
                   <Route path="/codeblock/:codeBlockTitle" element={<CodeBlock />} />
-                  <Route path="/editor" element={<MyPage />} /> {/* New route for MyPage */}
+                  <Route path="/editor" element={<CodeEditor />} /> 
               </Routes>
           </Router>
       </div> 
