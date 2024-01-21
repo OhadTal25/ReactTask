6f2d71b718755bc534029afe34968f6f0a1d1aad
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-01-21 13:18:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lobby from './components/Lobby.js';
 import CodeBlock from './components/CodeBlock.js';
-import MyPage from './components/CodeEditor.js'; 
+import CodeEditor from './components/CodeEditor.js'; 
 import './App.css'; 
 import './components/Lobby.css'; 
 import './components/CodeBlock.css'; 
